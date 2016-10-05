76e4b6d146b1da5172db3d642e0f40eec7d3533d
kilhalverson <kilhalverson@mail.roanoke.edu>
2016-10-04 20:30:16 -0400

    started beginUI() on UI.cc

--- UserInterface.cc
+++ UserInterface.cc
@@ -3,3 +3,24 @@
 #include <iostream>
 
 using namespace std;
+
+//PRE: The node has been fully integrated into the network
+//     and is ready to start storing and finding keys.
+//     isRunning is a boolean passed by reference to indicate
+//     the interface is currently running.
+//POST: The user will now have the option to enter input
+//      into the user interface.
+void beginUI(bool & isRunning){
+  //while(isRunning){
+  try{
+
+    //run UI
+    
+
+  }
+  catch(string ErrorMessage){
+    cout << ErrorMessage << endl;
+  }
+  //}
+}
+
