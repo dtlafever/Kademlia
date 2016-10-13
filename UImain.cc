b8f7c5d614f63af73c804f6d057e8a4c142a2f85
KimiLois <kilhalverson@mail.roanoke.edu>
2016-10-13 17:20:12 -0400

    changed UI

--- UImain.cc
+++ UImain.cc
@@ -5,10 +5,15 @@
 
 using namespace std;
 
-//PRE:
-//POST:
+//PRE: 
+//POST: The user starts the User Interface and is asked to
+//      input the IP address of the node it wants to speak to.
+//      The UI object will be created and a UDP socket will be
+//      connected to the given IP address. 
 int main(){
 
+  string givenIP;
+  //ASSERT: IP for the node we want to talk to
   
   UserInterface UI = UserInterface();
   //ASSERT: create a UserInterface object for the user
