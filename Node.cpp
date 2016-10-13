88e16f05ff21c467dc4a8311ce4816f56f8c4718
IngyN <ingyesa@aucegypt.edu>
2016-10-13 16:55:14 -0400

    deleting useless functions

--- Node.cpp
+++ Node.cpp
@@ -14,58 +14,6 @@ uint32_t Node::getMyID() {
 	return ID;
 }
 
-//PRE: the node we want to ping
-//POST: checks to make sure that node is still alive. Removes from the k-bucket
-//      if dead node
-void Node::ping(uint32_t nodeID) {
-	
-}
-
-//PRE: the key that represents our file
-//POST: Finds the distance (ID XOR key) and sends the store command to the k closest nodes
-void Node::findCandidatesForStore(uint32_t key) {
-	
-}
-
-//PRE: the key that represents our file
-//POST: adds the key to our list of keys
-void Node::store(uint32_t key) {
-	keys.push_back(key);
-}
-
-//PRE: the node ID we are looking for in our network
-//POST: Starts by picking ALPHA nodes from the closest non-empty bucket. Then call findNode
-//      on each of those nodes asyncronously. Nodes that fail to respond quickly (TIME_TO_RESPOND)
-//      are removed from consideration until and unless they do respond. If these nodes fail to return
-//      nodes that are closer than the closest we have already seen, resend findNode to another ALPHA number
-//      of nodes from our list up until k times.
-void Node::nodeLookup(uint32_t nodeID) {
-	
-}
-
-//PRE: the key of the file we want to lookup
-//POST: acts the same as the nodeLookup, but now we return true if we can find the key, false otherwise.
-bool Node::valueLookup(uint32_t keyID) {
-	return 1;
-}
-
-//PRE: a node ID we want to find in the network
-//POST: returns up to k nodes that are closest to the requested nodeID.
-//NOTE: if a node fails to respond, they will get removed for the coorsponding bucket
-//      until and unless they respond. additionally, the node we are looking for is
-//      updated in the appropriate bucket for this node so we can find it later
-//      (including adding to our bucket if it does not exist).
-//NOTE: by return we mean sending a message
-void Node::findNode(uint32_t nodeID) {
-	
-}
-
-//PRE: a key we want to find in the network
-//POST: acts the same as findNode, but now we return true if we have found a value
-//NOTE: by return we mean sending a message
-void Node::findValue(uint32_t key) {
-	
-}
 
 //---------------------------------------------------------------------------------
 //            PUBLIC FUNCTIONS
