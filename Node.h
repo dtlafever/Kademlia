5a13bf4b83b445d5c5bdcdab1817c60bac9338d0
jcgiguere <jcgiguere@mail.roanoke.edu>
2016-10-26 18:05:47 -0400

    node constructor so far

--- Node.h
+++ Node.h
@@ -1,16 +1,21 @@
 #define PORT 6666
 
-class Node{
+class Node {
 
  private:
+  
   RoutingTable RT;
   vector<int> keys();
-  int ID;
+  uint32_t ID;
   bool inNetwork;
   
  public:
 
-  Node(int id);
+
+  //Pre: id is a valid node id that is not yet taken
+  //Post: RT is initalized, ID = id, inNetwork = true
+  //      RT is empty since this node is starting a new network
+  Node(uint32_t id) : RT(id);
 
   Node(int id, int contactID, int contactIP);
 
