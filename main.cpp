45301454dfad70e23254138157d3687febafc4db
jcgiguere <jcgiguere@mail.roanoke.edu>
2016-10-25 21:52:12 -0400

    Adjusted main.cpp, should be good, currently I have it printing out the error, change to throw if we want it to be that way.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,41 @@
-int main(int argc, char * argv[]){
+//Main Program for Starting a Kademlia Node
 
-  //first arg == ID
-  //second arg == contact ID
-  //third arg == contact IP
-  if (argc == 2){ //creating new network
-    Node n(argv[1]); //assume convert to number
-    n.startListener();
-  }else if (argc == 4){
-    Node n(argv[1], argv[2], argv[2]);
-    if (n.joined()){
-      n.startListener();
+#include "node.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MY_ID 1
+#define CONTACT_ID 2
+#define CONTACT_IP 3
+
+#define CREATE_NETWORK 2
+#define JOIN_NETWORK 4
+
+//Pre: argc = 2 or 4, if 4 then argv[2], argv[3] represents an existing node
+//     argv[1] is a valid node id not yet taken
+//Post: Throws an error if the above is not met or failed to join the network
+//      At this stage, just prints error, adjust if we want too
+int main(int argc, char * argv[]){
+  uint32_t myID = atoi(argv[MY_ID]);
+  if (argc == CREATE_NETWORK) {
+    Node newNode(myID);
+    newNode.startListener();
+  }
+  else if (argc == JOIN_NETWORK) {
+    uint32_t contactID = atoi(argv[CONTACT_ID]);
+    uint32_t contactIP = atoi(argv[CONTACT_IP]);
+    Node newNode(myID, contactID, contactIP);
+    if (newNode.joined()) {
+      newNode.startListener();
     }
     else{
-      //ERROR OUT
+      //ERROR OUT (join error)
+      printf("This node has failed to join the network \n");
     }
   }
-  
-  return 0;
+  else {
+    //Error Out (argument error)
+    printf("Invalid Arguments were given \n");
+  }
+  return (0);
 }
