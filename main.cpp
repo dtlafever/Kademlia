f93475600abdfeecee10058bd89e1a81f1387f81
IngyN <ingyesa@aucegypt.edu>
2016-10-28 03:00:42 -0400

    Hoping to convert ip... come on derek!

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char * argv[]){
   else if (argc == JOIN_NETWORK) {
     uint32_t contactID = atoi(argv[CONTACT_ID]);
 		
-		uint32_t contactIP = parseIPV4string(argv[CONTACT_IP]);
+		uint32_t contactIP = getIPInt(string(argv[CONTACT_IP]));
     Node newNode(myID, contactID, contactIP);
     if (newNode.joined()) {
       newNode.startListener();
